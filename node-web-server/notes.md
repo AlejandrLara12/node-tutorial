cb623a42a4d4392c7f59d3e6bbbc45162343641c
Ricardo Lara <alejandro.elara@live.com>
2017-12-06 12:32:01 -0600

    Advanced Templeting

--- node-web-server/notes.md
+++ node-web-server/notes.md
@@ -1,3 +1,13 @@
 # Hello Express
 
 # Creating a Web Server
+
+# Advanced Templeting
+terminal
+nodemon server.js -e js,hbs
+-e  extencios to be looking at
+
+nom i -g nodemon; upates nodemon
+
+We can have partials, which are like templates
+We can create functions named 'helpers' and we can use them inside templates 
--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,22 +1,39 @@
 const express = require('express');
+const hbs = require('hbs');
 
 let app = express();
 
 //middlewear
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => { // request, response
   // res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: 'Ricardo',
-    likes: [12, 'hockey']
+  // res.send({
+  //   name: 'Ricardo',
+  //   likes: [12, 'hockey']
+  // });
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome',
+    // currentYear: new Date().getFullYear()
   });
 });
 
 app.get('/about', (req, res) => { // request, response
-  res.send('<h2>About Page</h2>');
+  // res.send('<h2>About Page</h2>');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    // currentYear: new Date().getFullYear()
+  });
 });
 
 app.get('/bad', (req, res) => { // request, response
