4dff5954080fe0e49a2aa3291f055fa153054bb1
R Lara <alejandro.elara@live.com>
2017-12-05 20:05:27 -0600

    Adding promises to main process

---
+++ weather-app/app-primise.js
@@ -0,0 +1,45 @@
+const yargs = require('yargs');
+const axios = require('axios');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true // always parse it as a stirng
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+//console.log(argv);
+
+let encodedAddress = encodeURIComponent(argv.address);
+let geocodeUrl = `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+axios.get(geocodeUrl)
+  .then((response) => {
+    if(response.data.status === 'ZERO_RESULTS'){
+      throw new Error('Unable to find that address.');
+    }
+    //console.log(JSON.stringify(response.data, undefined,2));
+    console.log(response.data.results[0].formatted_address);
+    let lat = response.data.results[0].geometry.location.lat;
+    let lng = response.data.results[0].geometry.location.lng;
+    let weatherUrl = `https://api.darksky.net/forecast/48865be6755a85dcac5b5300db92d572/${lat},${lng}`;
+    
+    return axios.get(weatherUrl);
+  })
+  .then((response) => {
+    let temperature = response.data.currentry.temperature;
+    let apparentTemperature = response.data.currentry.apparentTemperature;
+
+  })
+  .catch((e) => {
+    if(e.code === 'ENOTFOUND'){
+      console.log('Unable to connect to API servers');
+    } else {
+      console.log(e.message);
+    }
+  });
