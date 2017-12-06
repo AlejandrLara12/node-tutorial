fecfec154dd2304de7346804c92c54486608413a
Ricardo Lara <alejandro.elara@live.com>
2017-12-05 23:29:40 -0600

    Basics express

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,1 +1,23 @@
 const express = require('express');
+
+let app = express();
+
+app.get('/', (req, res) => { // request, response
+  // res.send('<h1>Hello Express!</h1>');
+  res.send({
+    name: 'Ricardo',
+    likes: [12, 'hockey']
+  });
+});
+
+app.get('/about', (req, res) => { // request, response
+  res.send('<h2>About Page</h2>');
+});
+
+app.get('/bad', (req, res) => { // request, response
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000);
